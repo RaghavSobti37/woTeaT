fa8295710e7313140088e0a70daa33c3ee9a1091
RaghavSobti37 <raghavsobti37@gmail.com>
2026-05-27 11:41:04 +0530

    making vercel work

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL || process.env.POSTGRES_URI || 'postgres://postgres:password@localhost:5432/woteat', {
   dialect: 'postgres',
+  dialectModule: pg,
   logging: false,
 });
 
