f04ed7b7226a131b5ecbd18b178052a055ad2722
RaghavSobti37 <raghavsobti37@gmail.com>
2026-05-27 11:10:29 +0530

    fix: vercel build config

--- api/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(process.env.POSTGRES_URI || 'postgres://postgres:password@localhost:5432/woteat', {
-  dialect: 'postgres',
-  logging: false,
-});
-
-module.exports = sequelize;
