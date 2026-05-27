d6d35c70470a1f01b1adfaad965700ef4a924617
RaghavSobti37 <raghavsobti37@gmail.com>
2026-05-27 11:36:58 +0530

    fix: use POSTGRES_URL from vercel integration

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.POSTGRES_URI || 'postgres://postgres:password@localhost:5432/woteat', {
+const sequelize = new Sequelize(process.env.POSTGRES_URL || process.env.POSTGRES_URI || 'postgres://postgres:password@localhost:5432/woteat', {
   dialect: 'postgres',
   logging: false,
 });
